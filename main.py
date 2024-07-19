4120d14cb364625224ae5e1175b1bfd658dbd5c3
abedmus <160133466+abedmus@users.noreply.github.com>
2024-07-20 02:09:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from ai import respond
-from tts import speak
+import google.generativeai as genai
+import os
+from gtts import gTTS
+from playsound import playsound
+
+API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=str(API_KEY))
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+def respond(message):
+    response = model.generate_content(message)
+    return response.text
+
+def speak(text):
+    tts = gTTS(text, lang='en', tld='us')
+    filename = "voice"+".mp3"
+    tts.save(filename)
+    playsound(filename)
+    os.remove(filename)
 
 while True:
     print("Listening...")
     message = input()
     if message.strip():
         response = respond(message)
+        print(response)
         speak(response)
